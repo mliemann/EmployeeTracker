f172ba7b996795ed4dc563b764a9f0adb9e257c0
Marlon Liemann <awesomemarlon@gmail.com>
2021-07-21 08:41:52 -0400

    created schema

---
+++ banktrack.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS bankDB;
+CREATE database bankDB;
+
+USE bankDB;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMA,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
